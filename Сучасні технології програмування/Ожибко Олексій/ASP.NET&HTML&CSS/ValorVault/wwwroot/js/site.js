ec797a7c0576a02ec2e240289e803f795fc0c783
bibizyan <59194291+bibizyank4@users.noreply.github.com>
2025-11-29 17:54:38 +0200

    Add files via upload

---
+++ Сучасні технології програмування/Ожибко Олексій/ASP.NET&HTML&CSS/ValorVault/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿const userProfilesContainer = document.querySelector('.user-profiles');
+
+// Запит до бази даних для отримання профілів користувачів
+fetch('https://your-api-endpoint/get-user-profiles')
+    .then(response => response.json())
+    .then(data => {
+        // Перебір масиву даних з профілями користувачів
+        data.forEach(profile => {
+            // Створення HTML-коду для картки користувача
+            const userProfileHTML = `
+                <div class="user-profile">
+                    <h2>${profile.name}</h2>
+                    <p>Email: ${profile.email}</p>
+                    <p>Створено анкет: ${profile.createdSurveys}</p>
+                    <p>Опубліковано анкет: ${profile.publishedSurveys}</p>
+                    <p>Відхилено анкет: <span class="math-inline">\{profile\.rejectedSurveys\}</p\>
+<button data\-user\-id\="</span>{profile.id}">Видалити</button>
+                </div>
+            `;
